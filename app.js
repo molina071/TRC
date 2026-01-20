f3d1ecf0695029795c6471b861e65c6f507f63e5
molina071 <7josueortega111@gmail.com>
2026-01-20 08:57:53 -0600

    librerias

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/Conexion');
+//const userController = require('./controlador/usuarioControlador');
+const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//set up view engine
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+//app.set('views', path.join(__dirname, 'views'));
+
+
+//routes//
+//index
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+/*// Create an user
+app.get('/user/create', userController.createUsersForm);
+app.post('/user/create', userController.createUsers);
+// Update an user
+app.get('/user/update/:id', userController.updateUsersForm);
+app.post('/user/update/:id', userController.updateUsers);
+// Delete an usuario
+app.post('/user/delete/:id', userController.deleteUser);*/
+
+// Start server
+const PORT = process.env.PORT || 3000;
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
