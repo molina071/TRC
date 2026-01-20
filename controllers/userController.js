4d35cc0f4d7c1cad246899c7ac10ef922595157b
molina071 <7josueortega111@gmail.com>
2026-01-20 15:34:27 -0600

    dashboard culminado

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const user = require('../models/userModel');
+const user = require('../models/userModel'); //ESTE USER CONTROLER DEBE DE SER EDITADO
 
 const userController = {
 
