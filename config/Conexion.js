d15f74e919c5754f920b602148f4b0d1bc2426e9
molina071 <7josueortega111@gmail.com>
2026-01-24 17:55:35 -0600

    ajuste de puerto
    ajuste de puerto a estandar 3306

--- config/Conexion.js
+++ config/Conexion.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('sistema_viajes', 'root', '', {
     host: "localhost",
     dialect: 'mysql',
-  
+
 });
 
 module.exports = sequelize;
