3e7013e354e52138fe8d5c05f2c2dc6edc729e32
molina071 <7josueortega111@gmail.com>
2026-01-24 12:04:44 -0600

    puerto
    puerto editado a 3036

--- config/Conexion.js
+++ config/Conexion.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('sistema_viajes', 'root', '', {
     host: "localhost",
     dialect: 'mysql',
-    port: 3307,
+  
 });
 
 module.exports = sequelize;
